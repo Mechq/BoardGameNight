54b90f425288a540937dc7c3ccaef9f9009e008c
Mech <stefrensma@gmail.com>
2025-10-19 16:13:56 +0200

    Swagger on production enabled

--- API/Program.cs
+++ API/Program.cs
@@ -51,13 +51,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
    
-}
+
 
 app.UseHttpsRedirection();
 app.MapGraphQL();
